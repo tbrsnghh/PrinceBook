7db75117b46ea163dabc80ed08f6f1da64ae0fd4
vanhaaaa <anhpubg2003@gmail.com>
2024-09-18 16:59:02 +0700

    fix1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,10 @@
-import Banner from "./components/banner/BannerTest";
-import Header from "./components/header/Header";
+
 import Account from "./pages/account/Account";
 import BookDetailPage from "./pages/bookDetail/BookDetailPage";
 import Cart from "./pages/cart/Cart";
 import Home from "./pages/home/Home";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
-
-import Header from "./components/admin/components/Header";
-import Dasborad from "./components/admin/components/Dasborad"
-import Home from "./components/admin/components/Home";
-import ListUser from "./components/admin/components/DanhMuc/ListDm";
-import AddUsera from "./components/admin/components/DanhMuc/ListDm";
-import Footer from "./components/admin/components/Footer";
 function App() {
   return (
     <div className="App">
