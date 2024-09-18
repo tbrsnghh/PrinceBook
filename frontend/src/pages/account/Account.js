403d0f06be4f5ce70fb3c2361a1f8d97dd89df3c
Tbrsnghh <nhham1771@gmail.com>
2024-09-18 16:19:41 +0700

    security

---
+++ frontend/src/pages/account/Account.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Header from '../../components/header/Header'
+
+export default function Account() {
+  return (
+    <>
+        <Header/>
+        <h1>Account Page</h1>
+
+        
+    </>
+  )
+}
