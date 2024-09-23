db2c5e0839aa21d0f1f50f1f64f0598c796deb7c
vanhaaaa <anhpubg2003@gmail.com>
2024-09-23 10:19:09 +0700

    .

--- backend/src/main/java/com/example/backend/repositories/OrderRepository.java
+++ backend/src/main/java/com/example/backend/repositories/OrderRepository.java
@@ -1,12 +1,12 @@
-package com.example.backend.repositories;
+// package com.example.backend.repositories;
 
-import com.example.backend.models.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+// import com.example.backend.models.Order;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
 
-import java.util.List;
+// import java.util.List;
 
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findByUserId (Long userId);
-}
+// @Repository
+// public interface OrderRepository extends JpaRepository<Order, Long> {
+//     // List<Order> findByUserId (Long userId);
+// }
--- backend/src/main/java/com/example/backend/services/OrderService.java
+++ backend/src/main/java/com/example/backend/services/OrderService.java
@@ -1,40 +1,40 @@
-package com.example.backend.services;
+// package com.example.backend.services;
 
-import com.example.backend.dtos.OrderDTO;
-import com.example.backend.models.Order;
-import com.example.backend.repositories.OrderRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+// import com.example.backend.dtos.OrderDTO;
+// import com.example.backend.models.Order;
+// import com.example.backend.repositories.OrderRepository;
+// import lombok.RequiredArgsConstructor;
+// import org.springframework.stereotype.Service;
 
-import java.util.List;
+// import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class OrderService implements IOrderService{
-    private final OrderRepository orderRepository;
+// @Service
+// @RequiredArgsConstructor
+// public class OrderService implements IOrderService{
+//     private final OrderRepository orderRepository;
 
-    @Override
-    public Order saveOrder(OrderDTO orderDTO) {
-        return null;
-    }
+//     @Override
+//     public Order saveOrder(OrderDTO orderDTO) {
+//         return null;
+//     }
 
-    @Override
-    public Order getOrderById(Long id) {
-        return null;
-    }
+//     @Override
+//     public Order getOrderById(Long id) {
+//         return null;
+//     }
 
-    @Override
-    public Order updateOrder(Long id, OrderDTO orderDTO) {
-        return null;
-    }
+//     @Override
+//     public Order updateOrder(Long id, OrderDTO orderDTO) {
+//         return null;
+//     }
 
-    @Override
-    public void deleteOrder(Long id) {
+//     @Override
+//     public void deleteOrder(Long id) {
 
-    }
+//     }
 
-    @Override
-    public List<Order> findByUserId(Long userId) {
-        return List.of();
-    }
-}
+//     @Override
+//     public List<Order> findByUserId(Long userId) {
+//         return List.of();
+//     }
+// }
