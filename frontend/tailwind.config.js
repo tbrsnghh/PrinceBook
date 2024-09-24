d388695b04cf8c3307a7acb367e2da13a5ef727e
Tbrsnghh <nhham1771@gmail.com>
2024-09-24 17:07:56 +0700

    fix frontend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/layout/**/*.{html,js}",
+    "./src/components/**/*.{html,js}",
+    "./src/pages/**/*.{html,js}"
+    
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
