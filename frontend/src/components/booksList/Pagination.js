6175de21f99c625078b55d86a35342b1d73656e3
Tbrsnghh <nhham1771@gmail.com>
2024-09-26 13:53:24 +0700

    ~

--- frontend/src/components/booksList/Pagination.js
+++ frontend/src/components/booksList/Pagination.js
@@ -1,29 +1,36 @@
 import React from "react";
 import "./pagination.scss";
 export default function Pagination({ currentPage, totalPages, onPageChange }) {
-  // const handlePrevious = () => {
-  //   if (currentPage > 1) {
-  //     onPageChange(currentPage - 1);
-  //   }
-  // };
+  const handlePrevious = () => {
+    if (currentPage > 0) {
+      onPageChange(currentPage - 1);
+    }
+  };
 
-  // const handleNext = () => {
-  //   if (currentPage < totalPages) {
-  //     onPageChange(currentPage + 1);
-  //   }
-  // };
+  const handleNext = () => {
+    if (currentPage < totalPages -1 ) {
+      onPageChange(currentPage + 1);
+    }
+  };
 
   return (
     <div className="join">
+      <button className="join-item btn" onClick={handlePrevious}>
+      Previous
+      </button>
       {[...Array(totalPages)].map((_, i) => (
         <button
-          key={i + 1}
-          className={`join-item btn ${currentPage === i ? "btn-active" : ""}`}
+          key={i}
+          className={`join-item btn ${currentPage === i  ? "btn-active" : ""}`}
           onClick={() => onPageChange(i)}
         >
           {i + 1}
         </button>
       ))}
+      <button className="join-item btn" onClick={handleNext}>
+          Next
+      </button>
     </div>
   );
 }
+
