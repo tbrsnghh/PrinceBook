b9a8d6484839a5541003e7a5a89113b6d5a5c9d0
hoanghaonguyen <haohao6345@gmail.com>
2024-09-26 16:32:07 +0700

    ud CheckoutComp

--- frontend/src/components/checkout/CheckoutComp.js
+++ frontend/src/components/checkout/CheckoutComp.js
@@ -6,20 +6,24 @@ import { postOrder } from "../../store/orderSlice";
 import { removeItemFromCart } from "../../store/cartSlice";
 import { postOrderDetail } from "../../store/orderDetailSlice";
 import { useLocation, useNavigate } from "react-router-dom";
+import emailjs from "@emailjs/browser"; // Cập nhật import
 
 const CheckoutComp = () => {
-  const location = useLocation();  
-  const order_now = location.state?.order; 
+  const location = useLocation();
+  const order_now = location.state?.order;
 
-  const dispatch = useDispatch(); const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const cart = useSelector((state) => state.cart);
   const user = useSelector((state) => state.user);
-  
-  
-  const items_order = order_now ? [order_now ] : cart.cartItems.filter((item) => item.checked);
+
+  const items_order = order_now
+    ? [order_now]
+    : cart.cartItems.filter((item) => item.checked);
   const total_Price = order_now ? order_now.total : cart.totalMoney;
   const user_info = user.user;
   console.log(items_order);
+
   const [note, setNote] = useState("");
   const [shipping_address, setShippingAddress] = useState("");
   const [paymentMethod, setPaymentMethod] = useState("cash");
@@ -32,7 +36,7 @@ const CheckoutComp = () => {
     phone_number: user_info.phone,
     total_Price: total_Price,
     shipping_method: "",
-    shipping_address: "",
+    shipping_address: user_info.address,
     shipping_date: "",
     payment_method: "",
   });
@@ -45,13 +49,14 @@ const CheckoutComp = () => {
       payment_method: paymentMethod,
     }));
   }, [note, shipping_address, paymentMethod]);
+
   const handleThanhToan = async () => {
     const resultAction = await dispatch(postOrder(order)).unwrap();
     if (resultAction.error) {
       console.log(resultAction.error);
       return;
     }
-  
+
     items_order.forEach((item) => {
       dispatch(
         postOrderDetail({
@@ -64,6 +69,41 @@ const CheckoutComp = () => {
       );
       dispatch(removeItemFromCart(item.id));
     });
+
+    // Gửi email sau khi hoàn tất thanh toán
+    const templateParams = {
+      from_name: "Prince Book",
+      to_email: order.email,
+      to_name: order.username,
+      message: `
+        <h1>Cam on ban da ung ho PrinceBook. Prince Book chan thanh cam on ban!</h1>
+        <h3>Your Information: </h3>
+        <p>Tong tien: ${total_Price}</p>
+        <p>Thanh toan: ${paymentMethod}</p>
+        <p>Ngay dat hang: ${new Date().toLocaleDateString()}</p>
+      `,
+    };
+
+    emailjs
+      .send(
+        "service_1z0i5cp",
+        "template_26srqcd",
+        templateParams,
+        "aKdSpH011sM0d1DmZ"
+      )
+      .then(
+        (response) => {
+          console.log(
+            "Email sent successfully!",
+            response.status,
+            response.text
+          );
+        },
+        (err) => {
+          console.error("Failed to send email. Error: ", err);
+        }
+      );
+
     setTimeout(() => {
       navigate("/ok");
     }, 1000);
@@ -94,4 +134,3 @@ const CheckoutComp = () => {
 };
 
 export default CheckoutComp;
-
