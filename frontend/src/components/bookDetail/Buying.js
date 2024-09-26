bac5ffdabb9a6b5541a89f9eaee3123e1f21b93e
hoanghaonguyen <haohao6345@gmail.com>
2024-09-26 10:59:49 +0700

    ud Buying

--- frontend/src/components/bookDetail/Buying.js
+++ frontend/src/components/bookDetail/Buying.js
@@ -3,32 +3,32 @@ import Offers from "./Offers";
 import { useDispatch } from "react-redux";
 import { addItemToCart } from "../../store/cartSlice";
 import { useNavigate } from "react-router-dom";
-export default function Buying({book}) {
-  const [quantity, setQuantity] = useState(1)
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+export default function Buying({ book }) {
+  const [quantity, setQuantity] = useState(1);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const addItem = () => {
     const item = {
       id: book.id,
       name: book.name,
       price: book.price,
       quantity: quantity,
-      checked: false
-    }
-    dispatch(addItemToCart(item))
-  }
-  const handleBuyNow = () => {  
-    const order = {  
-      id: book.id,  
-      name: book.name,  
-      price: book.price,  
-      quantity: quantity,  
+      checked: false,
+    };
+    dispatch(addItemToCart(item));
+  };
+  const handleBuyNow = () => {
+    const order = {
+      id: book.id,
+      name: book.name,
+      price: book.price,
+      quantity: quantity,
       total: quantity * book.price,
-    };  
+    };
 
-    navigate("/checkout", { state: { order }}); // Sử dụng state để gửi đơn hàng  
-};
-  
+    navigate("/checkout", { state: { order } }); // Sử dụng state để gửi đơn hàng
+  };
+
   return (
     <div className="bg-gray-100 p-4 rounded-lg">
       {/* className="w-1/3 fixed right-0 top-20 h-full bg-white p-4 shadow-lg" */}
@@ -37,19 +37,39 @@ export default function Buying({book}) {
       <div className="mt-4 flex items-center ">
         <span className="font-bold">Số lượng:</span>
         <div className="flex items-center ml-2">
-          <button className="px-2 py-1 bg-gray-200 rounded-l-lg"
-            onClick={() => setQuantity(quantity > 1 ? quantity - 1 : 1)}>-</button>
-          <span className="w-12 text-center border-t border-b border-gray-200">{quantity}</span>
-          <button className="px-2 py-1 bg-gray-200 rounded-r-lg"
-            onClick={() => setQuantity(quantity + 1)}>+</button>
+          <button
+            className="px-2 py-1 bg-gray-200 rounded-l-lg"
+            onClick={() => setQuantity(quantity > 1 ? quantity - 1 : 1)}
+          >
+            -
+          </button>
+          <span className="w-12 text-center border-t border-b border-gray-200">
+            {quantity}
+          </span>
+          <button
+            className="px-2 py-1 bg-gray-200 rounded-r-lg"
+            onClick={() => setQuantity(quantity + 1)}
+          >
+            +
+          </button>
         </div>
       </div>
-      <div className="mt-4 flex">  
-        <span className="font-bold">Tạm tính: </span>  
-        <span className="block text-lg font-semibold ml-2">151.000₫</span>  
-      </div>   
-      <button className="button-add-to-cart" onClick={addItem}>Thêm vào giỏ hàng</button>
-      <button className="button-add-to-cart" onClick={handleBuyNow}>Mua ngay</button>
+      <div className="mt-4 flex">
+        <span className="font-bold">Tạm tính: </span>
+        <span className="block text-lg font-semibold ml-2 text-red-500">          {book.price
+            ? new Intl.NumberFormat("vi-VN", {
+                style: "currency",
+                currency: "VND",
+              }).format(book.price * quantity)
+            : "Đang cập nhật"}
+        </span>
+      </div>
+      <button className="button-add-to-cart" onClick={addItem}>
+        Thêm vào giỏ hàng
+      </button>
+      <button className="button-add-to-cart" onClick={handleBuyNow}>
+        Mua ngay
+      </button>
     </div>
   );
 }
